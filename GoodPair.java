b4a6b263efed2f4a72c5e59dab0deee07cf7edc3
Kartik Arora <67683793+BowlPulp@users.noreply.github.com>
2024-08-24 06:48:29 +0530

    Create GoodPair.java

---
+++ GoodPair.java
@@ -0,0 +1,7 @@
+for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
